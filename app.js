3e0f33d219f9aaca33468b4de544e1dc83154fa3
Umwizer <ruthumwizerwa@gmail.com>
2024-04-17 10:19:37 +0200

    Adding new features

--- app.js
+++ app.js
@@ -1,69 +1,69 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
-const Blog = require('./models/blog.models.js')
+const Blog = require('./models/blog.models.js');
+
 //express app
 const app = express();
 
 //connect to mongoDB
-const dbURI ='mongodb+srv://ruthumwizerwa:fI2OJ3zp7dWmt2O9@cluster0.ykpdugp.mongodb.net/';
+const dbURI ='mongodb+srv://ruthumwizerwa32:LyP99JosZ8g79iD7@cluster0.xrryyvn.mongodb.net/'
 mongoose.connect(dbURI)
-.then(() => {
-  console.log('Connected to MongoDB');
-  // Start the Express server after successful connection
-  app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-  });
-})
-.catch(err => {
-  console.error('\x1b[31mError connecting to MongoDB:', err.message);
-});
-//register  view engine
-app.set("view engine", "ejs");
-
-//listen for requests
-// app.listen(3000);
-
-app.use(morgan('tiny')); 
-// mongoose and mongo sandbox routes
-app.get('/add-blog', (req, res) => {
-const blog = new Blog({
-  title:'new blog',
-  snippet:'Learn using net ninja',
-  body:'more about new blog'
-});
-blog.save().then((result)=>{
-  res.send(result);
-})
-.catch((err) => {
-console.log(err);
-})
-})
-//middleware and static files
-app.use(express.static('public'))
-app.get("/", (req, res) => {
-  //res.send('<p>home page</p>');
-  const blogs =[
-{title:'Ruth Neeeds to learn react',snippet:'Learn using net ninja'},
-{title:'Ruth Neeeds to learn new development',snippet:'Learn using net ninja'},
-{title:'Ruth Neeeds to learnnews',snippet:'Learn about new news'}
-  ]
-  res.render("index",{title:'home page',blogs:blogs});
-});
-app.use((req,res,next) => {
-  console.log('in the next middleware')
-  next()
+  .then(() => {
+    console.log('Connected to MongoDB');
   })
-
-  app.get("/about", (req, res) => {
-    res.render("about",{title:'about'});
+  .catch(err => {
+    console.error('\x1b[31mError connecting to MongoDB:', err.message);
   });
-  // redirects
-  app.get('/blogs/create', (req, res) => {
-    res.render('create',{title:'create new blog'});
+
+// Start the Express server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+//register view engine
+app.set("view engine", "ejs");
+//middleware and static files
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('tiny'));
+
+app.get("/", (req, res) => {
+  res.redirect('/blogs');
+});
+
+app.use((req, res, next) => {
+  console.log('in the next middleware');
+  next();
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: 'about' });
+});
+
+//blogs routes
+app.get('/blogs', (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.render('index', { title: 'All blogs', blogs: result });
     })
-//404 page
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    });
+}); 
+
+app.post('/blogs',(req,res) => {
+console.log(req.body)
+res.send('Data received')
+})
+// redirects
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'create new blog' });
+});
 
 //404 page
 app.use((req, res) => {
-  res.status(404).render("404",{title:'404 page'});});;
+  res.status(404).render("404", { title: '404 page' });
+});
